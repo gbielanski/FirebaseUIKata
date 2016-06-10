8451db3d6875516686b0893543bb2ca8d96694e1
Grzegorz Bielanski <gbielanski@poczta.onet.pl>
2016-06-10 13:49:09 +0200

    Create Firebase object

--- app/src/main/java/pl/kata/bielanski/firebaseuikata/MainActivity.java
+++ app/src/main/java/pl/kata/bielanski/firebaseuikata/MainActivity.java
@@ -7,7 +7,9 @@ import com.firebase.client.Firebase;
 
 import butterknife.ButterKnife;
 
+
 public class MainActivity extends AppCompatActivity {
+	Firebase mRootRef;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +17,6 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_main);
 		ButterKnife.bind(this);
 		Firebase.setAndroidContext(this);
+		mRootRef = new Firebase("https://glowing-torch-4628.firebaseio.com/BusStopList");
 	}
 }
