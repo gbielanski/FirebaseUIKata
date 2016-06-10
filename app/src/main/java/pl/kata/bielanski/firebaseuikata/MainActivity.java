f1afb9a574434ec8e0a267d9d16e61d4844a77ca
Grzegorz Bielanski <gbielanski@poczta.onet.pl>
2016-06-10 13:46:17 +0200

    Init Butterknife and Firebase

--- app/src/main/java/pl/kata/bielanski/firebaseuikata/MainActivity.java
+++ app/src/main/java/pl/kata/bielanski/firebaseuikata/MainActivity.java
@@ -3,11 +3,17 @@ package pl.kata.bielanski.firebaseuikata;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.firebase.client.Firebase;
+
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		ButterKnife.bind(this);
+		Firebase.setAndroidContext(this);
 	}
 }
